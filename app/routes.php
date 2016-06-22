161f7d0133520819ec0f7a6fd2515d16c0917b26
Anastasia Oudin Nikokosheva <a.nikokosheva@gmail.com>
2016-06-22 16:06:32 +0200

    routes conflict

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,6 @@
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'default_home'],
 		['GET|POST', '/event/[:id]', 'Event#showEvent', 'event_showEvent'],
-		// ['GET|POST', '/list', 'List#showList', 'list_showList'],
 		['GET|POST', '/register', 'User#register', 'user_register'],
+		['GET|POST', '/login', 'User#login', 'user_login'],
 	);
