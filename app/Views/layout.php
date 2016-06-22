5552b8b2fba1d8e408e56918c8265c7d2c8cf781
Baptiste <baptistecousin78@gmail.com>
2016-06-22 14:35:48 +0200

    footer

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -3,14 +3,16 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?= $this->e($title) ?></title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"> <!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"> <!-- Font awesome -->
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>"> <!-- Style Css -->
 
 </head>
 <body>
 	<div class="container">
 		<header>
-			<h1>Limonade :: Plateforme d'organisation d'événements</h1>
+			<h1>Limonade :: Stop la prise de tête pour organiser votre événements</h1>
+			<h4>Posez vous, sirotez votre verre et laissez notre site faire</h4>
 			<h2><?= $this->e($title) ?></h2>						
 		</header>
 
@@ -22,14 +24,19 @@
 	<footer>
     	<!-- Nous contacter -->
 		<div class="team">
+
+
 		</div>
+
 		<!-- Nous suivre -->
 		<div class="social-network">
+
 		</div>
+
 	</footer>
 		
 	</div>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script> <!-- Jquery -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script> <!-- Boostrap Js -->
 </body>
 </html>
