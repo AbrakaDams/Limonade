9c353351250b5e5cbd3e1dd714d8b9b124349d4f
Baptiste <baptistecousin78@gmail.com>
2016-06-22 17:17:44 +0200

    contact

--- app/Views/default/contact.php
+++ app/Views/default/contact.php
@@ -1,14 +1,14 @@
 <?php $this->layout('layout', ['title' => 'Contact']) ?>
 
 <?php $this->start('main_content') ?>
-<h2>Contact</h2>
+
 
 <div class="row">
   <div class="col-xs-10 col-md-2">
     <div class="thumbnail">
-      <img src="" alt="..." class="img-circle" height="100px" width="100px">
+      <img src="../public/assets/img/avatar/Anastasia.jpg" alt="..." class="img-circle" height="100px" width="100px">
       <div class="caption">
-        <h3></h3>
+        <h3>Anastasia Nikokosheva Oudin</h3>
         <p></p>
         <p><a href="#" class="btn btn-primary" role="button">Contacter</a> </p>
       </div>
@@ -16,10 +16,9 @@
   </div>
   <div class="col-xs-10 col-md-2">
     <div class="thumbnail">
-      <img src="" alt="..." class="img-circle" height="100px" width="100px">
+      <img src="../public/assets/img/avatar/Damien.jpg" alt="..." class="img-circle" height="100px" width="100px">
       <div class="caption">
-        <h3>
-        </h3>
+        <h3>Damien Machado</h3>
         <p></p>
         <p><a href="#" class="btn btn-primary" role="button">Contacter</a> </p>
       </div>
@@ -27,9 +26,9 @@
   </div>
   <div class="col-xs-10 col-md-2">
     <div class="thumbnail">
-      <img src="" alt="..." class="img-circle" height="100px" width="100px">
+      <img src="../public/assets/img/avatar/Myriam.jpg" alt="..." class="img-circle" height="100px" width="100px">
       <div class="caption">
-        <h3></h3>
+        <h3>Myriam Khalfi Bugnazet</h3>
         <p></p>
         <p><a href="#" class="btn btn-primary" role="button">Contacter</a> </p>
       </div>
@@ -37,9 +36,9 @@
   </div>
   <div class="col-xs-10 col-md-2">
     <div class="thumbnail">
-      <img src="" alt="..." class="img-circle" height="100px" width="100px">
+      <img src="../public/assets/img/avatar/Baptiste.jpg" alt="..." class="img-circle" height="100px" width="100px">
       <div class="caption">
-        <h3></h3>
+        <h3>Baptiste Cousin</h3>
         <p></p>
         <p><a href="#" class="btn btn-primary" role="button">Contacter</a> </p>
       </div>
@@ -47,15 +46,15 @@
   </div>
   <div class="col-xs-10 col-md-2">
     <div class="thumbnail">
-      <img src="" alt="..." class="img-circle" height="100px" width="100px">
+      <img src="../public/assets/img/avatar/Noé.jpg" alt="..." class="img-circle" height="100px" width="100px">
       <div class="caption">
-        <h3></h3>
+        <h3> Noé Champigny</h3>
         <p></p>
         <p><a href="#" class="btn btn-primary" role="button">Contacter</a> </p>
       </div>
     </div>
   </div>
 </div>
-
+<a href="<?= $this->url('default_home') ?>">Retour Accueil</a>
 
 <?php $this->stop('main_content') ?>
