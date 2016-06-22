6479f29abe5a79761262576e7fe31318085b32a3
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-06-22 12:41:35 +0200

    changement route

--- app/routes.php
+++ app/routes.php
@@ -3,5 +3,5 @@
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'default_home'],
 		['GET|POST', '/event/[:id]', 'Event#showEvent', 'event_showEvent'],
-		['GET|POST', '/register', 'User#register', 'default_register'],
+		['GET|POST', '/register', 'User#register', 'user_register'],
 	);
