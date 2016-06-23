3c68776c10cf278bbb444c810923c996d8ebe431
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-06-23 17:08:39 +0200

    barsearch

--- app/Views/event/search.php
+++ app/Views/event/search.php
@@ -1,6 +1,17 @@
 <?php $this->layout('layout', ['title' => 'Affichage recherche']) ?>
 
 <?php $this->start('main_content') ?>
+<?php   echo '<div class="event-title">';
+		echo '<br>';				
+		echo '<h2>' .$search['title'] . '</h2>';
+		echo '<br><br>';
+		echo '<div class="event-desc">';
+		echo '<h2>' .$search['desc'] . '</h2>';
+		echo '<br><br>';	
+		echo '<div class="event-address">';
+		echo '<h2>' .$search['adress'] . '</h2>';
+		echo '<br><br>';	
+	
+?>
 
-
-<?php $this->stop('main_content') ?>
+<?php $this->stop('main_content') ?> 
