22910887bfac85736ad96cc4323a262084b9b568
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-06-23 15:25:42 +0200

    ajour searchbar dans evencontroller

--- app/Controller/EventController.php
+++ app/Controller/EventController.php
@@ -53,3 +53,23 @@ class EventController extends Controller
 	}
 
 }
+
+/**
+	 * Méthode pour searchbar
+	 */
+
+	// Effectue une recherche
+	// Le premier argument est un tableau associatif où la clé correspond à la colonne SQL
+	// Le second argument est l'opérateur OR ou AND pour la recherche
+
+	public function searchEvent()
+	{
+		$search = new EventModel();
+
+		$data =[
+		'search' => $post['search'],
+		];
+
+		$search->search($data);
+
+	}
