489a780ae44a12bfbfbe22e9530820e7d236fe86
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-06-23 11:24:05 +0200

    creation page de redirection après confirmation email

---
+++ app/Views/user/registerConfirm.php
@@ -0,0 +1,10 @@
+<?php $this->layout('layout', ['title' => 'Confirmation d\'email']) ?>
+
+<?php $this->start('main_content') ?>
+
+<div class="alert alert-success" role="alert">...</div>
+
+<a href="login.php">Bravo, vous avez bien confirmé votre email 
+<br> pour vous connecter cliquer ici!</a>
+
+<?php $this->stop('main_content') ?>
