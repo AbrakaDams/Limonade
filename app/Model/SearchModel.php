61ede5eb5cfa966da5d22dee8eb6f5cb988f0f36
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-06-23 15:10:12 +0200

    début searchbar crea search treat

---
+++ app/Model/SearchModel.php
@@ -0,0 +1,10 @@
+<?php
+namespace Model;
+
+class SearchModel extends \W\Model\Model
+{
+    // model name = table name in the database in lowercase without model
+    // BlogModel => blog_addArticle
+    // BlogUtilisateurs => blog_utilisateurs
+}
+
