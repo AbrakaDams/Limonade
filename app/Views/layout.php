5a0570594525c82d581234eda36555f51833514e
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-06-23 12:43:18 +0200

    layout creation barnav

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -9,12 +9,54 @@
 
 </head>
 <body>
-	<div class="container">
-		<header>
-			<h1>Limonade :: Stop la prise de tête pour organiser votre événements</h1>
-			<h4>Posez vous, sirotez votre verre et laissez notre site faire &#9786;</h4>
-			<h2><?= $this->e($title) ?></h2>
-		</header>
+	<header>
+		<!-- ma navbar -->
+		<nav class="navbar navbar-default">
+	  		<div class="container-fluid">
+	   			<!-- Brand and toggle get grouped for better mobile display -->
+	    		<div class="navbar-header">
+	     			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				        <span class="sr-only">Toggle navigation</span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				     </button>
+	      			<a class="navbar-brand" href="#">LIMONADE</a>
+	    		</div>
+	    <ul class="nav navbar-nav navbar-right">
+	   		<!--  searchbar -->
+			<form class="navbar-form navbar-left" role="search">
+				<div class="form-group">
+				  <input type="text" class="form-control" placeholder="Search">
+				</div>
+			<button type="submit" class="btn btn-default">Submit</button>
+			</form>
+			<!-- add list -->
+	      	<li><a href="#ajouter une liste"><i class="glyphicon glyphicon-plus"></i></a></li>
+	      	<!-- notifications -->
+	      	<li><a href="#notification"><i class="glyphicon glyphicon-bell"></i></a></li>
+	      	<!-- identité -->
+	        <li><a href="#"> Mon pseudo </a></li>
+	        <li><a href="#"><img class="logo" src="avatar"></a></li>
+
+	        <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> mon compte <span class="caret"></span></a>
+	          <ul class="dropdown-menu">
+	            <li><a href="#">créer un nouvel événement </a></li>
+	            <li><a href="#">paramètres </a></li>
+	            <li><a href="#">assistance </a></li>
+	            <li><a href="#">signalez un problème</a></li>
+	            <li><a href="#">deconnexion</a></li>
+	          </ul>
+	        </li>
+	      </ul>
+	  </div><!-- /.container-fluid -->
+	</nav>
+	<h1>Limonade :: Stop la prise de tête pour organiser votre événements</h1>
+	<h4>Posez vous, sirotez votre verre et laissez notre site faire &#9786;</h4>
+	<h2><?= $this->e($title) ?></h2>
+	
+	</header>
 
 
 		<section>
@@ -58,4 +100,5 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/3.0.3/handlebars.min.js"></script>
 	<script src="<?= $this->assetUrl('js/myscript.js') ?>"></script>
 </body>
+
 </html>
