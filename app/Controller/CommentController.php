e18f28f102c4ea89dd3743de70e483f1bf55186b
AbrakaDams <damien.machado33@gmail.com>
2016-06-23 13:55:17 +0200

    up comment

--- app/Controller/CommentController.php
+++ app/Controller/CommentController.php
@@ -15,4 +15,19 @@ class CommentController extends Controller
 
     return $showComment;
   }
+  public function insertComments(){
+    $post = [];
+    $error = [];
+
+    if(!empty($_POST)){
+			foreach($_POST as $key => $value){
+				$post[$key] = trim(strip_tags($value));
+			}
+      if(empty($post['comment'])){
+        $error[] = 'Le commentaire ne peut pas etre vide';
+      }
+      if(count($error) === 0){
+      
+      }
+  }
 }
