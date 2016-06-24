6c9b666ff3c64bb04d5463f2db693a1f3f3cc424
AbrakaDams <damien.machado33@gmail.com>
2016-06-24 12:18:00 +0200

    up event accueil

--- app/Views/default/home_logged.php
+++ app/Views/default/home_logged.php
@@ -2,6 +2,37 @@
 
 <?php $this->start('main_content') ?>
 
-  <h2>blalblabla</h2>
+<?php foreach ($thisEvent as $value): ?>
+<div style="display:inline-block;" class="">
+  <h2><?php echo $value['title']; ?></h2>
+  <p>Evènement de type :<i><?php echo $value['category']; ?></i> et :<i><?php echo $value['role']; ?></i></p>
+  <br>
+  <p><?php echo $value['description']; ?></p>
+  <p>Rejoint nous : <?php echo $value['address']; ?></p>
+  <p>Ca commence a :<?php echo $value['date_start']; ?></p>
+  <p> et fini à : <?php echo $value['date_end']; ?></p>
+</div>
+<?php endforeach; ?>
+
+<div style="display:inline-block;" class="">
+  Créer un évenement
+</div>
+<br><br><hr><br><br>
+<?php foreach ($thisEvent as $value): ?>
+<div style="display:inline-block;" class="">
+  <h2><?php echo $value['title']; ?></h2>
+  <p>Evènement de type :<i><?php echo $value['category']; ?></i> et :<i><?php echo $value['role']; ?></i></p>
+  <br>
+  <p><?php echo $value['description']; ?></p>
+  <p>Rejoint nous : <?php echo $value['address']; ?></p>
+  <p>Ca commence a :<?php echo $value['date_start']; ?></p>
+  <p> et fini à : <?php echo $value['date_end']; ?></p>
+</div>
+<?php endforeach; ?>
+
+<div style="display:inline-block;" class="">
+  Créer un évenement
+</div>
+
 
 <?php $this->stop('main_content') ?>
