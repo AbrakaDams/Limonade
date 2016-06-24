2dc489f15bad4563c16989cfad31464c47250460
Noe <champignyn@gmail.com>
2016-06-24 17:39:20 +0200

    Update invite.php

--- app/Views/event/invite.php
+++ app/Views/event/invite.php
@@ -3,19 +3,64 @@
 <?php $this->start('main_content') ?>
 	
 <div id="room_fileds">
-	<h2>Inviter des amis à votre évènement
-    	<input type="button" class="btn btn-warning" id="more_fields" onclick="add_fields();" value="+ 1 ami">
-    </h2>
+<h2>Inviter des amis à votre évènement
+    <input type="button" class="btn btn-warning" id="more_fields" onclick="add_fields();" value="+ 1 ami"></h2>
     <div class="content" id="wrapper"> 
         <span>
-        	Ajouter un ami : <input type="text"> <button class="btn btn-success">Ajouter</button>
+        	Ajouter un ami : <input type="text" class="typeahead"> <button class="btn btn-success">Ajouter</button>
         </span>
     </div>
 </div>
-
-	<script type="text/javascript">
-		function add_fields() {
-    		document.getElementById('wrapper').innerHTML += '<br><br><span>Ajouter un ami : <input type="text">  <button class="btn btn-success">Ajouter</button>\r\n';
-		}
-	</script>
 <?php $this->stop('main_content') ?>
+
+<?php $this->start('js'); ?>
+<script type="text/javascript">
+	function add_fields() {
+		document.getElementById('wrapper').innerHTML += '<br><span>Ajouter un ami : <input type="text">  <button class="btn btn-success">Ajouter</button>\r\n';
+	}
+	
+	var substringMatcher = function(strs) {
+	  return function findMatches(q, cb) {
+	    var matches, substringRegex;
+
+	    // an array that will be populated with substring matches
+	    matches = ['test', 'test2', 'test3'];
+
+	    // regex used to determine if a string contains the substring `q`
+	    substrRegex = new RegExp(q, 'i');
+
+	    // iterate through the pool of strings and for any string that
+	    // contains the substring `q`, add it to the `matches` array
+	    $.each(strs, function(i, str) {
+	      if (substrRegex.test(str)) {
+	        matches.push(str);
+	      }
+	    });
+
+	    cb(matches);
+	  };
+	};
+
+	var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+	  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+	  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+	  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+	  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+	  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+	  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+	  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+	  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+	];
+
+	$('.typeahead').typeahead({
+	  hint: true,
+	  highlight: true,
+	  minLength: 1
+	},
+	{
+	  name: 'states',
+	  source: substringMatcher(states)
+	});
+
+</script>
+<?php $this->stop('js'); ?>
