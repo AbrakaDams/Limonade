62c7dd6f31ba96863e03024276b79feeb663e798
Baptiste <baptistecousin78@gmail.com>
2016-06-28 14:26:53 +0200

    login css

--- app/Views/user/login.php
+++ app/Views/user/login.php
@@ -9,7 +9,6 @@
 	<?php endif; ?>
 
 <?php  if(isset($w_user) && !empty($w_user)): ?>
-
   <br>
   <p>
     <strong>Vous êtes déjà connecté(e). <br><a href="<?= $this->url('default_home') ?>">Retour Accueil</a></strong>
@@ -19,23 +18,25 @@
 
 	<form method="post" class="form-inline">
     <div class="form-group" id="form-login">
-      <h2>Se connecter?</h2>
-  		<label for="email">Votre email :</label><br>
+      <h2 class="center">Se connecter?</h2>
+  		<label for="email" id="formContact">Votre email :</label><br>
   		<input class="form-control" type="email" id="email" name="email" placeholder="votre email">
   		<br><br>
-  		<label for="password">Votre mot de passe :<br><i class="fa fa-question-circle" aria-hidden="true"></i>
-      <a href="<?= $this->url('user_getNewPassword'); ?>">Mot de passe oublié?</a></label><br>
+  		<label for="password" id="formContact">Votre mot de passe :<br>
+      <a href="<?= $this->url('user_getNewPassword'); ?>">Mot de passe oublié?</a></label><i class="fa fa-question-circle" aria-hidden="true"></i><br>
   		<input class="form-control" type="password" id="password" name="password" placeholder="votre mot de passe">
   		<br><br>
   		<input class="form-control" type="submit" value="Connexion">
   		<br><br>
-    </div>
-    <div class="option">
-  		<a href="<?= $this->url('default_home') ?>">Retour Accueil</a>
+    <div class="option" id="option-contact">
+  		<i class="fa fa-home" aria-hidden="true"></i>
+      <a href="<?= $this->url('default_home') ?>">Retour Accueil</a>
       <br>
-  		<a href="<?= $this->url('user_register') ?>"><strong>Créer un compte</strong></a>
+  		<i class="fa fa-user-plus" aria-hidden="true"></i>
+      <a href="<?= $this->url('user_register') ?>"><strong>Créer un compte</strong></a>
     </div>  
-	</form>
+    </div>
+  </form>
 
 <?php  endif; ?>
 
