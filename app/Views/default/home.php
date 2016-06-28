4c3131167540d64781648dbbc9665a58bb4a1df3
Baptiste <baptistecousin78@gmail.com>
2016-06-28 11:21:51 +0200

    css slider

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -11,26 +11,33 @@
 	  	<ol class="carousel-indicators">
 	    	<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
 	    	<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+	    	<li data-target="#carousel-example-generic" data-slide-to="2"></li>	  	
 	  	</ol>
 	  	<!-- Wrapper for slides -->
 	  	<div class="carousel-inner" role="listbox">
-	    	<div class="item active" ">
-	      			<img src="<?= $this->assetUrl('img/slider/img_slider1.jpg') ?>" class="img-slide">
+	    	<div class="item" ">
+	      		<img src="<?= $this->assetUrl('img/slider/img_slider1.jpg') ?>" class="img-slide">
 	      		<div class="carousel-caption">
-							<h1  class="titleIntro">Avec <strong>Limonade</strong> stop la prise de tête pour organiser votre événements !</h1>
-							<h4 class="textIntro">Posez vous, sirotez votre verre et laissez nous faire. &#9786;</h4>
-	        		<h3 class="titleIntro">Un outil attractif et intuitif</h3>
-		    			<p class="textIntro">Avec notre plateforme Limonade, vous pouvez gérer tous vos événements le plus simplement du monde.Pour cela nous mettons à votre disposition une interface simple et intuitif accompagné d'outils nécessaires pour mener à terme vos projets tout en gagnant du temps, de l'énergie et de l'efficacité.</p>
-							<a href="<?= $this->url('user_register'); ?>" class="btn btn-default btn-lg active" role="button">S'inscrire</a>
+					<h1 class="titleIntro">Un outil attractif et intuitif</h1>					
+	        		<h3 class="titleIntro1"></h3>
+		    		<p class="textIntro">Avec notre plateforme Limonade, vous pouvez gérer tous vos événements le plus simplement du monde.Pour cela nous mettons à votre disposition une interface simple et intuitif accompagné d'outils nécessaires pour mener à terme vos projets tout en gagnant du temps, de l'énergie et de l'efficacité.</p>					
 	      		</div>
 	    	</div>
 	    	<div class="item">
 	      		<img src="<?= $this->assetUrl('img/slider/img_slider2.jpg') ?>" class="img-slide" >
 	      		<div class="carousel-caption">
-	        		<h3 class="titleIntro">Une organisation d'événements</h3>
-	    				<p class="textIntro">Organisez et pilotez tous vos événements de A à Z sur un même outil.<br>
-	    				Pour cela Limonade est la plateforme qu'il vous faut pour vos événements.Reposez vous et laissez nous faire.</p>
-							<a href="<?= $this->url('user_login'); ?>" class="btn btn-default btn-lg active" role="button">Se connecter</a>
+	      			<h1 class="titleIntro">Une organisation d'événements</h1>
+		        	<h3 class="titleIntro1">Organisez et pilotez tous vos événements de A à Z sur un même outil.</h3>
+		    		<p class="textIntro"><br>
+		    		Pour cela Limonade est la plateforme qu'il vous faut pour vos événements.<br>Reposez vous et laissez nous faire.</p>					
+	      		</div>
+	    	</div>
+	    	<div class="item active">
+	      		<img src="<?= $this->assetUrl('img/slider/img_slider3.jpg') ?>" class="img-slide" >
+	      		<div class="carousel-caption">
+	      			<h1 class="titleIntro">Un événement est un fait important, un fait marquant pour vous tout comme pour nous.</h1><br>	        			    		
+		    		<a href="<?= $this->url('user_login'); ?>" class="btn btn-default btn-lg active" role="button">Se connecter</a>
+		    		<a href="<?= $this->url('user_register'); ?>" class="btn btn-default btn-lg active" role="button">S'inscrire</a>		    							
 	      		</div>
 	    	</div>
 	  	</div>
