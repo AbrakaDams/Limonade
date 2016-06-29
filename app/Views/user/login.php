e9433c2c58f5679f3f9660545bbd4bda696ccd17
AbrakaDams <damien.machado33@gmail.com>
2016-06-29 18:57:34 +0200

    update css welcome motha fucka

--- app/Views/user/login.php
+++ app/Views/user/login.php
@@ -17,45 +17,35 @@
 
 <?php  else: ?>
 
-<div class="container row">
-  <form method="post">
-    <div class="left log">
-      
-    </div>
-    <div class="last-col">    
-      
-    
-        
-      </div>
-    </div>
-  </div>
 
 
-	<form method="post" class="form-inline">
-    <div class="form-group" id="form-login">    
-      <h2>Se connecter?</h2>
-  		<label for="email" id="formContact">Votre email :</label><br>
-  		<input class="form-control" type="email" id="email" name="email" placeholder="Votre email">
-  		<br><br>
-  		<label for="password" id="formContact">Votre mot de passe :<br>
-      <a href="<?= $this->url('user_getNewPassword'); ?>">Mot de passe oublié </a></label> <i class="fa fa-question-circle" aria-hidden="true"></i><br>
-  		<input class="form-control" type="password" id="password" name="password" placeholder="Votre mot de passe">
-  		<br><br>
-  		<input class="submit" type="submit" value="Connexion">
-  		<br><br><hr>
-    <div class="option" id="option-contact">
-  		<i class="fa fa-home" aria-hidden="true"></i>
-      <a href="<?= $this->url('default_home') ?>">Retour Accueil</a>
-      <br>
-  		<i class="fa fa-user-plus" aria-hidden="true"></i>
-      <a href="<?= $this->url('user_register') ?>"><strong>Créer un compte</strong></a>
-    </div>
-    </div>
-  </form>
-  <a href="<?=$this->url('user_loginFacebook');?>" class="btn btn-primary" style="border-radius:0;">
-      <i class="fa fa-facebook square"></i>
-      Connexion Facebook
-    </a>
+<form method="post" class="form-inline">
+	<div class="form-group" id="form-login">
+		<h2 class="centerNoConnect">Se connecter?</h2>
+		<label for="email" id="formContact">Votre email :</label><br>
+		<input class="form-control" type="email" id="email" name="email" placeholder="Votre email">
+		<br><br>
+		<label for="password" id="formContact">Votre mot de passe :<br>
+		<a href="<?= $this->url('user_getNewPassword'); ?>">Mot de passe oublié </a></label> <i class="fa fa-question-circle" aria-hidden="true"></i><br>
+		<input class="form-control" type="password" id="password" name="password" placeholder="Votre mot de passe">
+		<br><br>
+		<input class="submit" type="submit" value="Connexion">
+		<br><br>
+		<a href="<?=$this->url('user_loginFacebook');?>" class="btn btn-primary" style="border-radius:0;">
+		<i class="fa fa-facebook square"></i>
+		Connexion Facebook
+		</a>
+	<br><br><hr>
+		<div class="option" id="option-contact">
+			<i class="fa fa-home" aria-hidden="true"></i>
+			<a href="<?= $this->url('default_home') ?>">Retour Accueil</a>
+			<br>
+			<i class="fa fa-user-plus" aria-hidden="true"></i><a href="<?= $this->url('user_register') ?>"><strong>Créer un compte</strong></a>
+
+		</div>
+	</div>
+</form>
+
 
 <?php  endif; ?>
 
