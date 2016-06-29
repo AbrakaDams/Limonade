2332be3000a9873ac43b77fbb559a48e2bccfddc
Baptiste <baptistecousin78@gmail.com>
2016-06-29 12:24:24 +0200

    up

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -36,22 +36,5 @@
 	<?php endforeach; ?>
 </div>
 
-<hr>
-
-<div class="infos" id="infos">
-	<div class="informations" id="informations">
-		<h1 class="center">Informations</h1><hr>
-		<p>Vous souhaitez créer votre event sans vous prendre la tête? Vous êtes sur le bon site alors.<br>Avec l'aide de notre plateforme <strong>Limonade</strong> prenez en main la gestion de tous vos évènements.<br>
-		Un évènement est un fait important, un fait marquant pour vous tout comme pour nous.Il peut s'agir d'une soirée d'anniversaire pour votre enfant, une après-midi jeux de sociétés avec des inconnus afin de rencontrer de nouvelles personnes ou une journée à la plage avec vos amis.Tout le monde peut être concerné.<br>Organiser un évènement n'est pas toujours de tout repos à organiser, à gérer alors que ce doit être un moment de détente.
-		Pour cela notre plateforme vous propose des outils simples, et intuitifs pour la gestion de vos évènements.<br>Nous ne sommes pas une agence d'organisation d'évènements, nous mettons seulement à votre disposition une interface simple et intuitif pour que vos évènements soit simple à organiser, et que vous passiez un bon moment en compagnie de ceux qui vous sont chers.
-		<br>Avec une bonne maîtrise de notre plateforme vous pouvez créer de grands moments avec vos amis, dans votre vie tout en gagnant du temps, de l'énergie et l'efficacité.
-		</p>
-		<p>Nous sommes à votre disposition pour toutes vos questions.N'hésitez pas à nous contacter via notre <a href="<?= $this->url('contact_contact'); ?>">formulaire de contact</a>.</p>
-		<p>Si vous souhaitez rejoindre cette plateforme,<a href="<?= $this->url('user_register'); ?>">inscrivez-vous</a>, nous vous attendons.<br>
-		A très vite sur notre site.<br>
-		<br>Salutations de toute l'équipe de Limonade</p>
-	</div>
-</div>
-
 
 <?php $this->stop('main_content') ?>
--- app/Views/user/login.php
+++ app/Views/user/login.php
@@ -17,18 +17,32 @@
 
 <?php  else: ?>
 
+<div class="container row">
+  <form method="post">
+    <div class="left log">
+      
+    </div>
+    <div class="last-col">    
+      
+    
+        
+      </div>
+    </div>
+  </div>
+
+
 	<form method="post" class="form-inline">
-    <div class="form-group" id="form-login">
-      <h2 class="center">Se connecter?</h2>
+    <div class="form-group" id="form-login">    
+      <h2>Se connecter?</h2>
   		<label for="email" id="formContact">Votre email :</label><br>
-  		<input class="form-control" type="email" id="email" name="email" placeholder="votre email">
+  		<input class="form-control" type="email" id="email" name="email" placeholder="Votre email">
   		<br><br>
   		<label for="password" id="formContact">Votre mot de passe :<br>
-      <a href="<?= $this->url('user_getNewPassword'); ?>">Mot de passe oublié ?</a></label><i class="fa fa-question-circle" aria-hidden="true"></i><br>
-  		<input class="form-control" type="password" id="password" name="password" placeholder="votre mot de passe">
+      <a href="<?= $this->url('user_getNewPassword'); ?>">Mot de passe oublié </a></label> <i class="fa fa-question-circle" aria-hidden="true"></i><br>
+  		<input class="form-control" type="password" id="password" name="password" placeholder="Votre mot de passe">
   		<br><br>
-  		<input class="form-control" type="submit" value="Connexion">
-  		<br><br>
+  		<input class="submit" type="submit" value="Connexion">
+  		<br><br><hr>
     <div class="option" id="option-contact">
   		<i class="fa fa-home" aria-hidden="true"></i>
       <a href="<?= $this->url('default_home') ?>">Retour Accueil</a>
