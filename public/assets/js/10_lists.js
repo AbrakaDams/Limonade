e447f7e0b567fe22c58c44c742e03b20f220395e
Anastasia Oudin Nikokosheva <a.nikokosheva@gmail.com>
2016-07-01 18:22:10 +0200

    input values are added to modify-cards fileds

--- public/assets/js/10_lists.js
+++ public/assets/js/10_lists.js
@@ -61,11 +61,7 @@ var newCardEnd = '</select><br><input type="submit" value="Go"><input type="rese
 // newCard = newCardStart + createCardOptions(data.users) + newCardEnd
 var newCard;
 
-var modifyCardStart = '<span class="close-modify-card">Close</span><form class="modify-card-form" method="post"><label>Titre de cette tache</label><input type="text" name="card_title" maxlength="150" placeholder="Nom de votre nouvelle card"><br><label for="">Description</label><textarea name="card_desc"></textarea><br><label for="">Quantite</label><input type="number" name="card_quantity"><br><label for="">Prix</label><input type="number" name="card_price"><br><label for="">Responsable</label><select name="card_person"><option value="0">Choisir</option>';
-
-var modifyCardEnd = '</select><br><input type="submit" value="Go"><input type="reset" value="reset"></form>';
-
-var modifyCard;
+var modifyCardMiddle;
 // get all event participants
 $(document).ready(function() {
     $.ajax({
@@ -75,7 +71,7 @@ $(document).ready(function() {
         data: 'idEvent=' + thisEvent,
         success: function(data) {
             newCard = newCardStart + createCardOptions(data.users) + newCardEnd;
-            modifyCard = modifyCardStart + createCardOptions(data.users) + modifyCardEnd;
+            modifyCardMiddle = createCardOptions(data.users);
             getContent(lastDate);
         },
     });
@@ -185,7 +181,7 @@ function getContent(currentDate) {
                     var divToFind = 'div[data-id-list="'+value.id_list+'"]';
 
                     if($(dataToFind).length == 1) {
-                        $(divToFind).next().append('<div class="card" data-id-card="'+value.id+'"><h5 class="card-title">'+ value.card_title+'<span class="card-quantity"> &#x2715; '+value.quantity+'</span><span class="card-links"><a href="#" class="modify-card" data-modify-card="'+value.id+'"><i class="fa fa-pencil" aria-hidden="true"></i><span class="modify-card-container hidden">Modifier cette tache '+ modifyCard +'</span></a><a href="#" class="delete-card" data-delete-card="'+value.id+'"><i class="fa fa-times" aria-hidden="true"></i></a></span></h5><span class="card-price">Prix : '+value.price+' &#8364;</span><p class="card-desc">'+value.description+'</p><span class="card-responsable">'+value.username+' s\'en occupe</span></div>');
+                        $(divToFind).next().append('<div class="card" data-id-card="'+value.id+'"><h5 class="card-title">'+ value.card_title+'<span class="card-quantity"> &#x2715; '+value.quantity+'</span><span class="card-links"><a href="#" class="modify-card" data-modify-card="'+value.id+'"><i class="fa fa-pencil" aria-hidden="true"></i><span class="modify-card-container hidden">Modifier cette tache <span class="close-modify-card">Close</span><form class="modify-card-form" method="post"><label>Titre de cette tache</label><input type="text" name="card_title" maxlength="150" value="'+value.card_title+'" placeholder="Nom de votre nouvelle card"><br><label for="">Description</label><textarea name="card_desc">'+value.description+'</textarea><br><label for="">Quantite</label><input type="number" name="card_quantity" value="'+value.quantity+'"><br><label for="">Prix</label><input type="number" name="card_price" value="'+value.price+'"><br><label for="">Responsable</label><select name="card_person"><option value="0">Choisir</option>'+ modifyCardMiddle +'</select><br><input type="submit" value="Go"></form></span></a><a href="#" class="delete-card" data-delete-card="'+value.id+'"><i class="fa fa-times" aria-hidden="true"></i></a></span></h5><span class="card-price">Prix : '+value.price+' &#8364;</span><p class="card-desc">'+value.description+'</p><span class="card-responsable">'+value.username+' s\'en occupe</span></div>');
                     }
                 });
             }
@@ -240,11 +236,7 @@ $('#event-lists').on('click', '.delete-card', function(e) {
     })
 });
 
-// $('#event-lists').on('focusout', '.modify-list-form', function(e) {
-//     e.preventDefault();
-//     modifyList();
-// });
-//
+
 $(document).ready(function() {
     modifyList();
 });
