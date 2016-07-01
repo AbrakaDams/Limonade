bae3dfd207c5344ee5e072c4a086c3b0196dad25
Baptiste <baptistecousin78@gmail.com>
2016-07-01 16:56:05 +0200

    swag

--- app/Views/admin/backOffice.php
+++ app/Views/admin/backOffice.php
@@ -3,18 +3,6 @@
 <?php $this->start('main_content') ?>
 <h2>Bonjour</h2>
 
-<div class="listActions">
-	<ul>
-		<li>
-			<a href="<?= $this->url('admin_checkEvent'); ?>">Modifier un évènement</a>
-		</li>
-		<li>
-			<a href="<?= $this->url('admin_checkUser'); ?>">Modifier un utilisateur</a>
-		</li>		
-	</ul>
-</div>
-
-
 
 
 <?php $this->stop('main_content') ?>
--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -4,6 +4,19 @@
 <h2>Bonjour</h2>
 
 
+<div class="listActions">
+	<ul>
+		<li>
+			<a href="<?= $this->url('admin_checkEvent'); ?>">Modifier un évènement ?</a>
+		</li>
+		<li>
+			<a href="<?= $this->url('admin_checkUser'); ?>">Modifier un utilisateur ?</a>
+		</li>		
+	</ul>
+</div>
+
+
+
 
 
 
