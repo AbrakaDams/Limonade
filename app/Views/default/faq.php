00333a1665151af15bbddc1b874f769b332d6589
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-07-01 12:38:42 +0200

    faq css

--- app/Views/default/faq.php
+++ app/Views/default/faq.php
@@ -2,28 +2,59 @@
 <?php $this->layout('layoutNoConnect', ['title' => 'FAQ']) ?>
 
 <?php $this->start('main_content') ?>
-
-<section class="faq" id="faq">
-	<h1 class="center">Foire aux questions</h1>
-	<hr>
-	<h2 class="center">Questions fréquentes</h2>
-	<br>
-	<div class="questions" id="questions">
-		<h4>Comment modifier mes infos d'évènements?</h4>
-		<p>Après avoir créer votre event vous pourrez modifier toutes les informations concernant votre évènement sauf la portée(privée ou publique) et la catégorie.</p>
-		<br>
-		<h4>Comment créer un évènement ?</h4>
-		<p>A l'aide de notre interface, dans la partie <a href="<?= $this->url('event_createEvent'); ?>"> créer un évènement </a>ou avec le  <a href="<?= $this->url('event_createEvent'); ?>"><i class="glyphicon glyphicon-plus"></i></a>.</p>
-		<br>
-	</div>
-	<div class="questions" id="questions">
-		<h4>Comment inviter mes amis</h4>
-		<p>Ajouter participants après la création de votre évènement.</p>
-		<br>
-		<h4>Comment nous contacter?</h4>
-		<p>Via le formulaire de contact<a href="<?= $this->url('contact_contact'); ?>"> ici</a>.Nous sommes à votre disposition pour toutes questions</p>
-		<br>
-	</div>
-</section>
+<h1 class="center" id="top">Foire aux questions,</h1>
+<h2 class="center">les questions les plus fréquentes</h2>
+<hr>
+	
+<div class="faq">
+		<ul class="nav nav-pills nav-stacked">
+			<li role="presentation" class="active"><a href="#account">Pourquoi et comment créer mon compte?</a></li>
+			<li role="presentation" class="faqsection"><a href="#private-public">Quelle est la différence entre un événement privé et public?</a></li>
+			<li role="presentation" class="faqsection"><a href="#task">Mode d'emploi ajout des événements et des tâches</a></li>
+			<li role="presentation" class="faqsection"><a href="#comment">Laisser des commentaires à vos amis</a></li>
+			<li role="presentation" class="faqsection"><a href="#invite">Inviter des participants</a></li>
+		</ul>
+</div>
+<div class="reponses">
+	<ul class="faqList">
+		<li class="faqsection" id="account"><i class="fa fa-sign-in" aria-hidden="true"></i><br><br>
+		Pour gérer votre événement sur le site Lemonade vous devez avant tout tout vous connecter, celà vous permettra de garder tout vos projets d'organisation sur votre compte et d'inviter vos amis à mettre en place des projets avec vous! <br><hr>
+		<i class="fa fa-facebook-official" aria-hidden="true"></i><br>
+		Vous pouvez aussi vous inscrire avec facebook, vous recevrez un mot de passe pour vous connecter les fois suivantes. </li><hr>
+		<li class="faqsection" id="private-public"><i class="fa fa-circle" aria-hidden="true"></i><i class="fa fa-circle-o" aria-hidden="true"></i><br><br>
+		Un événement public sera visible par tous sur le site, il peut être très utile pour rassembler du monde (conférence, vernissage etc...)
+		Un événement privé n'est accessible que par vous et les amis que vous avez invités sur cet événement.</li><hr>
+		<li class="faqsection" id="task"><i class="fa fa-plus" aria-hidden="true"></i><br><br>
+		Pour ajouter un événement lorsque vous êtes en mode connectés vous n'avez qu'a cocher la petite croix d'ajout dévénement remplir les informations demandées, et enfin au sein d'une événement vous pourrez ajouter autant de taches que vous le souhaitez, elles seront utiles pour vous organiser.
+		Par exemple, nous partons au camping, la liste 1 comprendra le nombre de tente nécessaire et combien nous en avons, la liste 2 comprendra les sac de couchages, la liste 3 les litres d'eau à emporter etc..</li><hr>
+		<li class="faqsection" id="comment"><i class="fa fa-comment-o" aria-hidden="true"></i><br><br>
+		Vous pouvez à tout moment laisser un commentaire à vos amis en dessous des listes de tâches.</li><hr>
+		<li class="faqsection" id="invite"><i class="fa fa-user-plus" aria-hidden="true"></i><br><br>
+		Vous pouvez inviter des amis déjà inscrits sur Lemonade avec leurs pseudo</li>
+	</ul>
+<div>
+	
+	<a href="#top" id="icone"><i id="top" class="fa fa-hand-o-up" aria-hidden="true"></i></a></li>
+</div>
+</div>
 
 <?php $this->stop('main_content') ?>
+<?php $this->start('js') ?>
+<script type="text/javascript">
+	$(function () {
+	    $('a[href*="#"]:not([href="#"])').click(function () {
+	        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+	            var target = $(this.hash);
+	            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	            if (target.length) {
+	                $('html, body').animate({
+	                    scrollTop: target.offset().top
+	                }, 1000);
+	                return false;
+	            }
+	        }
+	    });
+	});
+</script>
+<?php $this->stop('js') ?>
+
