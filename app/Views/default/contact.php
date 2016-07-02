85839ac06e1f05c2a12675feb355d764bd270197
nikokosh <annikokosheva@yandex.ru>
2016-07-03 00:41:07 +0200

    style for contact us

--- app/Views/default/contact.php
+++ app/Views/default/contact.php
@@ -1,9 +1,12 @@
 <?php $this->layout('layout', ['title' => 'Contact']) ?>
 
 <?php $this->start('main_content') ?>
-<section id="contactSection">
+
+
+<div class="lemonade-bg" style="background-image: url(<?= $this->assetUrl('img/lemonade-1.jpg')?>)">
+
+    <div class="lemonade-bg-container">
      <!-- Formulaire de contact -->
-    <div class="my-form-contact">
         <form id="contact" method="post">
             <h2 class="center"><i class="fa fa-paper-plane" aria-hidden="true"></i> Formulaire de contact</h2><br>
 
@@ -25,27 +28,25 @@
 
         <?php endif; ?>
 
-            <div class="col-xs-6">
+            <div class="form-group two-inputs-aside beckille">
                 <label for="email" class="mail">Votre email :</label>
                 <input name="email" type="email" class="form-control" id="email" placeholder="Email">
-                <hr id="crazystupid-line">
             </div>
-            <div class="col-xs-6">
+            <div class="form-group two-inputs-aside">
                 <label for="name" class="name">Votre nom :</label>
                 <input name="name" type="text" class="form-control" id="name" placeholder="Nom">
-                <hr id="crazystupid-line">
             </div>
-         <br>
+
             <div class="form-group">
                 <label for="object" class="">L'objet de votre demande :</label>
                 <input name="object" type="text" class="form-control" id="object" placeholder="Objet">
-            <hr>
                 <label class="content">Votre contenu :</label>
                 <textarea name="content" class="form-control" rows="4" cols="3" placeholder="Votre contenu"> </textarea>
-            </div><br>
+            </div>
+
             <button type="submit" class="btn btn-primary">Envoyer le formulaire</button> &nbsp; <a href="<?= $this->url('default_home') ?>"><i class="fa fa-home fa-1x" aria-hidden="true"> Retour Accueil</i> </a>
         </form>
     </div>
-</section>
+</div>
 
 <?php $this->stop('main_content') ?>
