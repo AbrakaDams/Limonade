ed9e6de187504ead58608845ed6c2b7f97167adf
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-07-03 20:29:29 +0200

    css du l'admin du commentaire tavu

--- app/Views/admin/comments.php
+++ app/Views/admin/comments.php
@@ -1,32 +1,34 @@
 <?php $this->layout('layoutAdmin', ['title' => 'Admin']) ?>
 
 <?php $this->start('main_content') ?>
-<a href="<?= $this->url('admin_admin'); ?>" class="btn btn-default">Retour à l'accueil Admin</a>
-<h2><i class="fa fa-commenting-o" aria-hidden="true"></i>Liste des commentaires :</h2>
 
-<?php if(isset($comments) && !empty($comments)): ?>
-	<?php foreach ($comments as $comment): ?>
-		<b><?= $comment['content']; ?></b>
-		<a href="<?= $this->url('admin_supprComment', ['id' => $comment['id']]); ?>">Supprimer ce commentaire</a>
-		<br>
-		Ajouté le :
-		<?= $comment['date_add'];?>
-		Dans :
-		<a href="<?= $this->url('event_showEvent', ['id' => $comment['id_event']]); ?>">
-			<?= $comment['title']?>
-		</a>
-		Par :
-		<a href="<?= $this->url('admin_checkUser', ['id' => $comment['id_user']]); ?>">
-			<?= $comment['username'];?>
-		</a>
-		<br><br>
-	<?php endforeach; ?>
-<?php else: ?>
-	<p>
-		<strong> Pas de commentaires ...&nbsp; :'( </strong>
-	</p>
-<?php endif; ?>
-
+<div class="commentAdmin">
+	<a href="<?= $this->url('admin_admin'); ?>" class="btn btn-default">Retour à l'accueil Admin</a>
+	<h2><i class="fa fa-commenting-o" aria-hidden="true"></i>Liste des commentaires :</h2>
+	<br><hr>
+	<?php if(isset($comments) && !empty($comments)): ?>
+		<?php foreach ($comments as $comment): ?>
+			<b><?= $comment['content']; ?></b>
+			<a href="<?= $this->url('admin_supprComment', ['id' => $comment['id']]); ?>">Supprimer ce commentaire</a>
+			<br>
+			Ajouté le :
+			<?= $comment['date_add'];?>
+			Dans :
+			<a href="<?= $this->url('event_showEvent', ['id' => $comment['id_event']]); ?>">
+				<?= $comment['title']?>
+			</a>
+			Par :
+			<a href="<?= $this->url('admin_checkUser', ['id' => $comment['id_user']]); ?>">
+				<?= $comment['username'];?>
+			</a>
+			<br><hr>
+		<?php endforeach; ?>
+	<?php else: ?>
+		<p>
+			<strong> Pas de commentaires ...&nbsp; :'( </strong>
+		</p>
+	<?php endif; ?> 
+</div>	
 
 
 <?php $this->stop('main_content') ?>
