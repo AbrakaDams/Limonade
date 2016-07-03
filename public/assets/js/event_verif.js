5143883d0d6b9e4900b86f2d4d1841f554302c2b
nikokosh <annikokosheva@yandex.ru>
2016-07-03 11:17:34 +0200

    add card verif added, mais à tester

--- public/assets/js/event_verif.js
+++ public/assets/js/event_verif.js
@@ -8,17 +8,62 @@ $('#add-list-input').on('focusout, keyup', function() {
     }
 });
 
-$('.add-card-form input[name=card_title]').focusout(function() {
+
+$('#event-lists').on('focusout', '.add-card-form input[name=card_title]', function() {
     var cardTitle = $.trim($(this).val());
-    console.log(cardTitle);
-    if(addListInput.length > 50) {
+    if(cardTitle.length > 50 || cardTitle.length == 0) {
         markErrorInput(this);
-        console.log(cardTitle);
     }
     else {
-        markErrorInput(this);
+        markSuccessInput(this);
     }
 });
+
+
+$('#event-lists').on('focusout', '.add-card-form input[name=card_desc]', function() {
+    var cardTitle = $.trim($(this).val());
+    if(cardTitle.length > 50 || cardTitle.length == 0) {
+        markErrorInput(this);
+    }
+    else {
+        markSuccessInput(this);
+    }
+});
+
+$('#event-lists').on('focusout', '.add-card-form input[name=card_quantity]', function() {
+    var cardNum = $.trim($(this).val());
+
+    if(cardNum.length > 10 || cardNum < 0) {
+        markErrorInput(this);
+    }
+    else {
+        markSuccessInput(this);
+    }
+});
+
+$('#event-lists').on('focusout', '.add-card-form input[name=card_price]', function() {
+    var cardPrice = $.trim($(this).val());
+
+    if(cardPrice.length > 10 || cardPrice <= 0) {
+        markErrorInput(this);
+    }
+    else {
+        markSuccessInput(this);
+    }
+});
+
+// $('#event-lists').on('submit', '.add-card-form', function() {
+//     // console.log('HEhe');
+//     var formData = $(this).serialize();
+//     // console.log(formData);
+//     console.log(this);
+//     console.log($('input[name=card_title]').closest().val());
+//     if($('input[name=card_title]').closest().val().length == 0) {
+//         markErrorInput(this);
+//         console.log(this);
+//     }
+//
+// });
 
 // make particular input border red in case of failure
 function markErrorInput(input) {
