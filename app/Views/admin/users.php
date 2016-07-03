09b2c9a62fa9975df4d36677fb526951aec9b325
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-07-03 22:27:33 +0200

    css de admin users

--- app/Views/admin/users.php
+++ app/Views/admin/users.php
@@ -1,30 +1,35 @@
 <?php $this->layout('layoutAdmin', ['title' => 'Admin']) ?>
 
 <?php $this->start('main_content') ?>
-<a href="<?= $this->url('admin_admin'); ?>" class="btn btn-default">Retour à l'accueil Admin</a>
-<h2><i class="fa fa-users" aria-hidden="true"></i>Liste des utilisateurs :</h2>
 
-<?php foreach ($users as $user): ?>
-	<?php if($user['status'] == 'default'): ?>
-		<a href="<?= $this->url('admin_checkUser', ['id' => $user['id']]); ?>">
-			<?= $user['username']; ?>
-		</a>
-		<?= $user['firstname'].' '.$user['lastname'] ?>
-		<a href="<?= $this->url('admin_banUser', ['id' => $user['id']]); ?>">Ban</a>
-		<br>
-	<?php endif; ?>
-<?php endforeach; ?>
+<div class="usersAdmin">
+	<a href="<?= $this->url('admin_admin'); ?>" class="btn btn-default">Retour à l'accueil Admin</a>
+	<h2><i class="fa fa-users" aria-hidden="true"></i>Liste des utilisateurs :</h2>
 
-<h2>Liste des utilisateurs <strong> Bannis </strong> :</h2>
-<?php foreach ($users as $user): ?>
-	<?php if($user['status'] == 'banned'): ?>
-		<a href="<?= $this->url('admin_checkUser', ['id' => $user['id']]); ?>">
-			<?= $user['username']; ?>
-		</a>
-		<?= $user['firstname'].' '.$user['lastname'] ?>
-		<a href="<?= $this->url('admin_banUser', ['id' => $user['id']]); ?>">Unban</a>
-		<br>
-	<?php endif; ?>
-<?php endforeach; ?>
+	<?php foreach ($users as $user): ?>
+		<div class="list-users-admin">
+			<?php if($user['status'] == 'default'): ?>
+				<a href="<?= $this->url('admin_checkUser', ['id' => $user['id']]); ?>">
+					<?= $user['username']; ?>
+				</a>
+				<?= $user['firstname'].' '.$user['lastname'] ?>
+				<a href="<?= $this->url('admin_banUser', ['id' => $user['id']]); ?>">Ban</a>
+			<?php endif; ?>
+		</div>
+	<?php endforeach; ?>
+	
+	<h2>Liste des utilisateurs <strong> Bannis </strong> :</h2>
 
+	<?php foreach ($users as $user): ?>
+		<div class="list-users-ban">
+			<?php if($user['status'] == 'banned'): ?>
+				<a href="<?= $this->url('admin_checkUser', ['id' => $user['id']]); ?>">
+					<?= $user['username']; ?>
+				</a>
+				<?= $user['firstname'].' '.$user['lastname'] ?>
+				<a href="<?= $this->url('admin_banUser', ['id' => $user['id']]); ?>">Unban</a>
+			<?php endif; ?>
+		</div>
+	<?php endforeach; ?>
+</div>
 <?php $this->stop('main_content') ?>
