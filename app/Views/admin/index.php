8859306360d25ab8d574075b8255a67e9136d2a8
Millaa Millaa <contact.myriambugnazet@gmail.com>
2016-07-03 20:08:32 +0200

    css index admin just a begining

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -1,32 +1,35 @@
 <?php $this->layout('layoutAdmin', ['title' => 'Back Office']) ?>
 
 <?php $this->start('main_content') ?>
-<h2>Bienvenue sur l'interface d'administration du site</h2>
-<br>
 
-<h4><strong>Liste d'actions possibles</strong></h4>
+<div class="indexAdmin">
+	<h2 class="titleIndex">Bienvenue sur l'interface d'administration du site</h2>
+	<br>
 
-<div class="listActions">
-	<strong>
-		<ul>
-			<li>
-				<a href="<?= $this->url('admin_events'); ?>">Liste des &nbsp; <i class="fa fa-calendar-o" aria-hidden="true"></i> évènements ?</a>
-			</li>
-			<li>
-				<a href="<?= $this->url('admin_users'); ?>">Liste des  &nbsp; <i class="fa fa-users" aria-hidden="true"></i> utilisateurs ?</a>
-			</li>
-			<li>
-				<a href="<?= $this->url('admin_comments'); ?>">Liste des  &nbsp; <i class="fa fa-commenting-o" aria-hidden="true"></i> commentaires ?</a>
-			</li>
-			<li>
-				<a href="<?= $this->url('admin_messageConctact'); ?>">Liste des   &nbsp;  <i class="fa fa-comments-o" aria-hidden="true"></i> messages(contact) ?</a>
-			</li>
-			<br><br>
-			<li>
-				<a href="<?= $this->url('default_home'); ?>">Retour sur le site</a>
-			</li>
-		</ul>
-	</strong>
+	<h4 class="actionsIndex"><strong>Liste des actions possibles</strong></h4>
+
+	<div class="listActions">
+		<strong>
+			<ul class="listIndex">
+				<li>
+					<a href="<?= $this->url('admin_events'); ?>">Liste des &nbsp; <i class="fa fa-calendar-o" aria-hidden="true"></i> &nbsp; évènements..</a>
+				</li>
+				<li>
+					<a href="<?= $this->url('admin_users'); ?>">Liste des  &nbsp; <i class="fa fa-users" aria-hidden="true"></i> &nbsp; utilisateurs.. </a>
+				</li>
+				<li>
+					<a href="<?= $this->url('admin_comments'); ?>">Liste des  &nbsp; <i class="fa fa-commenting-o" aria-hidden="true"></i> &nbsp; commentaires..</a>
+				</li>
+				<li>
+					<a href="<?= $this->url('admin_messageConctact'); ?>">Liste des   &nbsp;  <i class="fa fa-comments-o" aria-hidden="true"></i> &nbsp; messages(contact)..</a>
+				</li>
+				<br><br>
+				<li>
+					<a href="<?= $this->url('default_home'); ?>">Retour sur le site</a>
+				</li>
+			</ul>
+		</strong> 
+	</div>
 </div>
 
 
